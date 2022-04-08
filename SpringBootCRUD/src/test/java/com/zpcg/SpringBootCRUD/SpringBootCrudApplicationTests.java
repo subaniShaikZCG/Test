dd5e5df4520be9a025016a623318e466e04b7029
Subani <Admin@host.docker.internal>
2022-04-08 14:25:32 +0530

    samoe changes done in profiles and test cases

--- SpringBootCRUD/src/test/java/com/zpcg/SpringBootCRUD/SpringBootCrudApplicationTests.java
+++ SpringBootCRUD/src/test/java/com/zpcg/SpringBootCRUD/SpringBootCrudApplicationTests.java
@@ -1,33 +1,27 @@
 package com.zpcg.SpringBootCRUD;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.awt.print.Printable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zpcg.SpringBootCRUD.Model.Employee;
-import com.zpcg.SpringBootCRUD.Repository.EmployeeDao;
 import com.zpcg.SpringBootCRUD.Service.EmployeeService;
 
 @WebMvcTest
